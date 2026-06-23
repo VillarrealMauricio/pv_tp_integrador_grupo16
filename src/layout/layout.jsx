bff4d4ca1488b1b5f677fa8ee60a639e8d5cadbd
Villarreal Mauricio Maximiliano <45765167@fi.unju.edu.ar>
2026-06-23 16:05:52 -0300

    feat: se agrego footer con formulario de consultas, redes sociales, y datos de contacto.

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,15 +1,18 @@
-import { Outlet } from 'react-router-dom';
 import Header from './Header';
+import Footer from './Footer'; 
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
     return (
-        <>
+        <div className="min-vh-100 d-flex flex-column bg-light" style={{ backgroundColor: '#f8f9fa' }}>
             <Header />
-            <main className="mt-4">
-                
+            
+            <main className="flex-grow-1">
                 <Outlet /> 
             </main>
-        </>
+
+            <Footer />
+        </div>
     );
 };
 
