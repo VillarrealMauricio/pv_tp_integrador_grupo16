ceb26827864eef600bac9bf0120345b2cfd16604
Seiseishon <francoocapo7@gmail.com>
2026-06-24 15:56:27 -0300

    feat: agregar pantalla de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './layout/Layout'; 
-import DashboardPrincipal from './views/DashboardPrincipal'; 
+import Layout from './layout/Layout';
+import DashboardPrincipal from './views/DashboardPrincipal';
 import ListaClientes from './views/ListaClientes';
+import { Login } from './views/Login';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route path='login' element={<Login />} />
         <Route index element={<Navigate to="/dashboard" replace />} />
         <Route path="dashboard" element={<DashboardPrincipal />} />
         <Route path="clientes" element={<ListaClientes />} />
